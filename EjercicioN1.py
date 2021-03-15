08c2cbbc63101cb602a391d060eab7ecc195a206
raulsanctes <raulsanctes@gmail.com>
2021-03-15 13:45:38 -0300

    Ejercicio 1 terminado

--- EjercicioN1.py
+++ EjercicioN1.py
@@ -1,17 +1,3 @@
-"""Ejercicio No1
-Crear un módulo para validación de nombres de usuarios. Dicho módulo, deberá
-cumplir con los siguientes criterios de aceptación:
-
---> El nombre de usuario debe contener un mínimo de 6 caracteres y un máximo de 12 OK
---> El nombre de usuario debe ser alfanumérico OK
---> Nombre de usuario con menos de 6 caracteres, retorna el mensaje “El nombre de
-    usuario debe contener al menos 6 caracteres” OK
---> Nombre de usuario con más de 12 caracteres, retorna el mensaje “El nombre de
-    usuario no puede contener más de 12 caracteres” OK
---> Nombre de usuario con caracteres distintos a los alfanuméricos, retorna el mensaje
-    “El nombre de usuario puede contener solo letras y números”
---> Nombre de usuario válido, retorna True """
-
 print("Ingrese su usuario")
 nombre = input()
 usuarioValido = nombre.isalnum()
