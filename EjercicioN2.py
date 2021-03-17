5b4d99fbead4d889b862dddb7f1877c6f22ab7fd
raulsanctes <raulsanctes@gmail.com>
2021-03-17 11:54:56 -0300

    Refactoreado y agregado colores

--- EjercicioN2.py
+++ EjercicioN2.py
@@ -1,79 +1,36 @@
-print("Ingrese su contraseña")
-password = input()
-enlistado = list(password)
-longitud = len(enlistado)
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
+def valida_pass(password):
 
-def error():
-    print("LA CONTRASEÑA ELEGIDA NO ES SEGURA")
-    return True
+    enlista = list(password)
+    if len(enlista) > 8 :
+        print(bcolors.OKGREEN +"Longitud OK")
+        if any(chr.isdigit() for chr in password):
+            print(bcolors.OKGREEN +"Digitos OK")
+            if any(chr.isalpha() for chr in password):
+                print(bcolors.OKGREEN +"Alfanumerico OK")
+                if any(chr.isalnum() != True for chr in password):
+                    print(bcolors.OKGREEN +"Caracter Especial OK")
+                    if any(chr.isspace() != True for chr in password):
+                        print(bcolors.OKGREEN +"No espacios OK")
+                        if any(chr.islower() for chr in password):
+                            print(bcolors.OKGREEN +"Minuscula OK")
+                            if any(chr.isupper() for chr in password):
+                                print(bcolors.OKGREEN +"Mayusculas OK")
+                        print(bcolors.OKCYAN + "CONTRASEÑA VALIDA" + bcolors.ENDC)
+                        return True
+    print(bcolors.WARNING + "Contraseña no válida" + bcolors.ENDC)
+    return False
 
+clave = input(bcolors.HEADER + "Ingrese una contraseña\n" + bcolors.ENDC)
 
-def valida_long():
-    if longitud >= 8:
-        return True
-    else:
-        print("LONGITUD NO VALIDA")
-        error()
-
-
-def valida_noalpha():
-    if any(chr.isalnum() == True for chr in password):
-        return True
-    else:
-        print("DEBE TENER AL MENOS UN CARACTER ALFANUMERICO")
-        error()
-
-
-def valida_alpha():
-    if any(chr.isalnum() != True for chr in password):
-        return False
-    else:
-        print("DEBE TENER AL MENOS UN CARACTER ESPECIAL")
-        error()
-
-
-def valida_minus():
-    if any(chr.islower() for chr in password):
-        return True
-    else:
-        print("DEBE TENER MINUSCULA")
-        error()
-
-
-def valida_mayus():
-    if any(chr.isupper() for chr in password):
-        return True
-    else:
-        print("DEBE TENER MAYUSCULA")
-        error()
-
-
-def valida_numero():
-    if any(chr.isdigit() for chr in password):
-        return True
-    else:
-        print("DEBE TENER UN NUMERO")
-        error()
-
-
-def valida_espacio():
-    for i in enlistado:
-        if i.isspace() != False:
-            print("NO USE ESPACIOS")
-            return True
-            error()
-
-
-def principal():
-    print(valida_long(), "LONGITUD")
-    print(valida_espacio(), "ESPACIO")
-    print(valida_alpha(), "ALFANUMERICO OK")
-    print(valida_noalpha(), "NO ALFANUMERICO")
-    print(valida_mayus(), "MAYUSCULA")
-    print(valida_minus(), "MINUSCULA")
-    print(valida_numero(), "NUMERO")
-    return "PASO"
-
-
-print(principal())
+print(valida_pass(clave))
