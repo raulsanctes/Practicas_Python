53096227f76721d115d3ac57a9486fa65af8665b
raulsanctes <raulsanctes@gmail.com>
2021-03-20 13:08:44 -0300

    Terminados

--- EjercicioN2.py
+++ EjercicioN2.py
@@ -1,41 +1,41 @@
 def clave(password):
 
-        validar=False #que se vayan cumpliendo los requisitos uno a uno.
-        long=len(password) #Calcula la longitud de la contraseña
-        espacio=False  #variable para identificar espacios
-        mayuscula=False #variable para identificar letras mayúsculas
-        minuscula=False #variable para contar identificar letras minúsculas
-        numeros=False #variable para identificar números
-        y=password.isalnum()#si es alfanumérica retona True
-        correcto=True #verifica que hayan mayuscula, minuscula, numeros y no alfanuméricos
+        validar=False 
+        long=len(password) 
+        espacio=False  
+        mayuscula=False
+        minuscula=False
+        numeros=False
+        y=password.isalnum()
+        correcto=True
         
-        for carac in password: #ciclo for que recorre caracter por caracter en la contraseña
+        for carac in password:
 
-            if carac.isspace()==True: #Saber si el caracter es un espacio
-                espacio=True #si encuentra un espacio se cambia el valor user
+            if carac.isspace()==True:
+                espacio=True
 
-            if carac.isupper()== True: #saber si hay mayuscula
-                mayuscula=True #acumulador o contador de mayusculas
+            if carac.isupper()== True: 
+                mayuscula=True 
                 
-            if carac.islower()== True: #saber si hay minúsculas
-                minuscula=True #acumulador o contador de minúsculas
+            if carac.islower()== True: 
+                minuscula=True 
                 
-            if carac.isdigit()== True: #saber si hay números
-                numeros=True #acumulador o contador de numeros
+            if carac.isdigit()== True:
+                numeros=True
                             
-        if espacio==True: #hay espacios en blanco
+        if espacio==True: 
                 print("La contraseña no puede contener espacios")
         else:
-            validar=True #se cumple el primer requisito que no hayan espacios
+            validar=True 
                        
         if long <8 and validar==True:
             print("Mínimo 8 caracteres")
-            validar=False #cambia a Flase si no se cumple el requisito móinimo de caracteres
+            validar=False 
 
         if mayuscula == True and minuscula ==True and numeros == True and y== False and validar ==True:
-           validar = True #Cumple el requisito de tener mayuscula, minuscula, numeros y no alfanuméricos
+           validar = True
         else:
-           correcto=False #uno o mas requisitos de mayuscula, minuscula, numeros y no alfanuméricos no se cumple
+           correcto=False
            
         if validar == True and correcto==False:
            print("La contraseña elegida no es segura: debe contener letras minúsculas, mayúsculas, números y al menos 1 carácter no alfanumérico")
